bf5d5df013e765f94c4bfb2976114760cda6940c
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-01-08 08:56:18 +0700

    add

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test{
+    public static void main(String[] args){
+        System.out.print("A");
+    }
+    public static void main(){
+        System.out.print("B");
+    }
+    public static void start(String[] args){
+        System.out.print("C");
+    }
+}
