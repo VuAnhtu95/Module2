99f936c8446ee7e2dcae4b108bf380bce4107a18
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-01-08 14:21:00 +0700

    add

---
+++ hienthiloichao.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class hienthiloichao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập name: ");
+        String name = scanner.nextLine();
+        System.out.printf("Hello %s",name);
+    }
+}
