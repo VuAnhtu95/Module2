34b901739d24835bd480c88e8b5a08df4a85b033
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-01-10 22:01:10 +0700

    add

---
+++ xoaphantu.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class xoaphantu {
+    public static void main(String[] args) {
+        int []arr = {4,12,7,8,6,2,6};
+        int x;
+        System.out.println("Đây là mảng trước khi xóa");
+        System.out.println(Arrays.toString(arr));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào đây số cần xóa");
+        x = scanner.nextInt();
+        int index_del = 0;
+        for (int i=0;i<arr.length;i++){
+            if (arr[i] == x){
+                index_del = i;
+                arr[i] = arr[index_del+1];
+                break;
+            }
+        }
+        System.out.println("Đây là mảng sau khi xóa "+Arrays.toString(arr));
+    }
+}
