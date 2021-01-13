2921b6839080525aaec94e0a820c5b9ce402ed1d
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-01-13 15:53:24 +0700

    add

---
+++ StaticMethod/Student.java
@@ -0,0 +1,17 @@
+package StaticMethod;
+
+public class Student {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+    Student(int r, String n) {
+        rollno = r;
+        name = n;
+    }
+    static void change() {
+        college = "CODEGYM";
+    }
+    void display() {
+        System.out.println(rollno + " " + name + " " + college);
+    }
+}
