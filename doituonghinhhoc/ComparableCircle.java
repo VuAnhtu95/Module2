8e7c3f926c12c9d958dc47037cd845a974afbc68
VuAnhtu95 <75408266+VuAnhtu95@users.noreply.github.com>
2021-01-17 22:24:33 +0700

    add

---
+++ doituonghinhhoc/ComparableCircle.java
@@ -0,0 +1,16 @@
+package doituonghinhhoc;
+
+public class ComparableCircle extends Circle implements Comparable <ComparableCircle>{
+    public ComparableCircle(){};
+    public ComparableCircle(double radius){
+        super(radius);
+    }
+    public ComparableCircle(double radius, String color, boolean filled) {
+        super(radius, color, filled);
+    }
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+    }
+}
---
+++ doituonghinhhoc/ShapeTest.java
@@ -0,0 +1,26 @@
+package doituonghinhhoc;
+
+public class ShapeTest {
+    public static void main(String[] args) {
+        Shape test = new Shape();
+        System.out.println(test);
+        test.setColor("red");
+        test.setFilled(false);
+        System.out.println(test);
+        Circle circle = new Circle();
+        System.out.println(circle);
+        circle.setRadius(3.5);
+        circle.setColor("pink");
+        circle.setFilled(false);
+        System.out.println(circle);
+        Rectangle rectangle = new Rectangle();
+        System.out.println(rectangle);
+        rectangle.setLength(30);
+        rectangle.setWidth(30);
+        rectangle.setColor("blue");
+        rectangle.setFilled(true);
+        System.out.println(rectangle);
+        Square square = new Square();
+        System.out.println(square);
+    }
+}
